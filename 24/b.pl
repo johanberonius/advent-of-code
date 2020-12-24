8b00361387c3190500565a1b84dfc04d19007090
Johan Beronius <johan.beronius@athega.se>
2020-12-24 22:26:47 +0100

    24 no z

--- 24/b.pl
+++ 24/b.pl
@@ -3,23 +3,22 @@ use strict;
 use List::Util qw(min max);
 
 my %d = (
-    e  => { x =>  1, y =>  0, z =>  1},
-    se => { x =>  1, y =>  1, z =>  0},
-    sw => { x =>  0, y =>  1, z =>  1},
-    w  => { x => -1, y =>  0, z => -1},
-    nw => { x => -1, y => -1, z =>  0},
-    ne => { x =>  0, y => -1, z => -1},
+    e  => { x =>  1, y =>  0 },
+    se => { x =>  1, y =>  1 },
+    sw => { x =>  0, y =>  1 },
+    w  => { x => -1, y =>  0 },
+    nw => { x => -1, y => -1 },
+    ne => { x =>  0, y => -1 },
 );
 my %t;
 my $c = 0;
 while (<>) {
     my @s = /(e|se|sw|w|nw|ne)/g;
 
-    my ($x, $y, $z) = (0, 0, 0);
+    my ($x, $y) = (0, 0);
     for my $s (@s) {
         $x += $d{$s}{x};
         $y += $d{$s}{y};
-        $z += $d{$s}{z};
     }
 
     $t{"$x,$y"} = !$t{"$x,$y"};
