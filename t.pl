bae6d216b5ab1d2ac048286bf4a15dad25d0bfdd
Johan Beronius <johan.beronius@athega.se>
2022-12-17 16:14:19 +0100

    Renamed repository and moved 2020

---
+++ t.pl
@@ -0,0 +1,16 @@
+
+use List::Util qw(min max product sum);
+
+
+use Math::Utils qw(lcm);
+
+
+use Time::HiRes qw(time);
+my $t1 = time();
+
+print "Time: ", time() - $t1, "\n";
+
+
+
+use Data::Dumper;
+print Dumper(\@r);
