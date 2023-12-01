06688a4324c3d8c026be4055a10f6d27c4a738a4
Johan Beronius <johan.beronius@athega.se>
2023-12-02 00:18:33 +0100

    2023/01 part 2: loop regexp

--- 2023/01/b2.pl
+++ 2023/01/b2.pl
@@ -2,4 +2,4 @@
 use List::Util qw(sum);
 
 %v=(one,1,two,2,three,3,four,4,five,5,six,6,seven,7,eight,8,nine,9);$r=join'|',keys%v;
-print sum map{s/$r/$v{$&}.substr$&,1/eg;s/$r/$v{$&}/g;@n=grep$_+0,split'';"$n[0]$n[-1]"}<>
+print sum map{0 while s/$r/$v{$&}.substr$&,1/eg;@n=grep$_+0,split'';"$n[0]$n[-1]"}<>
