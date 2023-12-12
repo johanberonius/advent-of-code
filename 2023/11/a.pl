fdd7a7993a0860b62834059fb045e0daca9ad3b2
Johan Beronius <johan.beronius@athega.se>
2023-12-12 20:56:30 +0100

    2023/11

---
+++ 2023/11/a.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+use strict;
+use List::Util qw(none);
+
+
+my @g;
+my ($x, $y) = (0, 0);
+my ($width, $height);
+
+while (<>) {
+    chomp;
+    $x = 0;
+    my $gx = 0;
+    for my $e (split '') {
+        if ($e eq '#') {
+            push @g => [$x, $y];
+            $gx++;
+        }
+        $x++;
+    }
+    $width = $x if $x > $width;
+    unless ($gx) {
+        print "No galaxies with y: $y\n";
+        $y++;
+    }
+    $y++;
+
+}
+$height = $y;
+
+print "Grid width: $width, height: $height\n";
+
+
+for (my $x = 0; $x < $width; $x++) {
+    if (none { $_->[0] == $x } @g) {
+        print "No galaxies with x: $x\n";
+        for (@g) {
+            $_->[0]++ if $_->[0] > $x;
+        }
+        $x++;
+        $width++;
+    }
+
+}
+
+print "Grid width: $width, height: $height\n";
+
+my $s = 0;
+my $p = 0;
+for my $i (0..$#g-1) {
+    for my $j ($i+1..$#g) {
+        my $d = abs($g[$i][0] - $g[$j][0]) + abs($g[$i][1] - $g[$j][1]);
+        print "Distance from galaxy $i ($g[$i][0], $g[$i][1]) to $j ($g[$j][0], $g[$j][1]) : $d\n";
+        $s += $d;
+        $p++;
+    }
+}
+
+print "Pairs: $p, total distance: $s\n";
