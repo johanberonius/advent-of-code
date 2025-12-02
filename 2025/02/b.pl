e592fc17183bba7dba4ab2742a617daacea51a89
Johan Beronius <johan.beronius@athega.se>
2025-12-02 19:36:31 +0100

    2025/02

---
+++ 2025/02/b.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+
+my @id = map [split '-'], split ',', <>;
+my $sum = 0;
+
+for (@id) {
+    my ($min, $max) = @$_;    
+    print "$min-$max invalid IDs: ";
+
+    for ($min .. $max) {
+        if (/^(\d+)\1+$/) {            
+            $sum += $_;
+            print "$_, ";
+        }
+    }
+    print "\n";
+}
+
+print "Sum of invalid IDs: $sum\n";
